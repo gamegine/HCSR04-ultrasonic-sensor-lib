93f7473d174ca0e9c124dc6a982b6ae8a58ecc16
gamgine <victor.fromentin95@ynov.com>
2018-01-29 16:18:40 +0100

    hcsr04.cpp multi sensor

--- src/HCSR04.cpp
+++ src/HCSR04.cpp
@@ -1,24 +1,28 @@
 #include "HCSR04.h"
 ////////////////////////////////////consttruct/destruct
-HCSR04::HCSR04(int out,int echo)
+void HCSR04::init(int out,int echo[])
 {
 	this->out = out;
 	this->echo = echo;
 	pinMode(this->out, OUTPUT);
-	pinMode(this->echo, INPUT);
+	for(int i=0;i<sizeof(echo);i++)
+	{ pinMode(this->echo[i], INPUT); }
 }
+HCSR04::HCSR04(int out,int echo){this->init(out,new int[1]{echo});}
+HCSR04::HCSR04(int out,int echo[]){this->init(out,echo);}
 HCSR04::~HCSR04()
 {
 	~this->out;
-	~this->echo;
+	delete[] this->echo;
 }
 ///////////////////////////////////////////////////dist
-float HCSR04::dist() const
+float HCSR04::dist(int n) const
 {
 	digitalWrite(this->out, LOW);
 	delayMicroseconds(2);
 	digitalWrite(this->out, HIGH);
 	delayMicroseconds(10);
 	digitalWrite(this->out, LOW);
-	return pulseIn(this->echo, HIGH) / 58.0;
+	return pulseIn(this->echo[n], HIGH) / 58.0;
 }
+float HCSR04::dist() const{return this->dist(0);}
