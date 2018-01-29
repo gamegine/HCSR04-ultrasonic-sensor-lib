fb1d5f810ab870231505ebb9f53db2835b88f9cd
gamgine <victor.fromentin95@ynov.com>
2018-01-29 17:10:27 +0100

    HCSR04_multi exemple

---
+++ examples/HCSR04_multi/HCSR04_multi.ino
@@ -0,0 +1,9 @@
+#include <HCSR04.h>
+
+HCSR04 hc(2,new int[6]{5,6,7,8,9,10});//initialisation class HCSR04 (trig pin , echo pin)
+
+void setup()
+{ Serial.begin(9600); }
+
+void loop()
+{ for (int i = 0; i < 6; i++ ) {Serial.println( hc.dist(i) );} }//return curent distance in serial for sensor 1 to 6
