c7a594c8791e84e745e5624affea05ec79c2969c
gamgine <victor.fromentin95@ynov.com>
2018-05-02 21:03:21 +0200

    add number of sensor in constructor multi

--- examples/HCSR04_multi/HCSR04_multi.ino
+++ examples/HCSR04_multi/HCSR04_multi.ino
@@ -1,6 +1,6 @@
 #include <HCSR04.h>
 
-HCSR04 hc(2,new int[6]{5,6,7,8,9,10});//initialisation class HCSR04 (trig pin , echo pin)
+HCSR04 hc(2,new int[6]{5,6,7,8,9,10},6);//initialisation class HCSR04 (trig pin , echo pin, number of sensor)
 
 void setup()
 { Serial.begin(9600); }
--- src/HCSR04.h
+++ src/HCSR04.h
@@ -3,13 +3,14 @@ class HCSR04
 {
 	public:
 	HCSR04(int out,int echo);				//initialisation class HCSR04 (trig pin , echo pin)
-	HCSR04(int out,int echo[]);				//initialisation class HCSR04 (trig pin , echo pin)
+	HCSR04(int out,int echo[],int n);		//initialisation class HCSR04 (trig pin , echo pin)
 	~HCSR04();								//destructor
 	float dist()      const;				//return curent distance of element 0
 	float dist(int n)      const;				//return curent distance of element 0
 
 	private:
-	void init(int out,int echo[]); 			//for constructor
+	void init(int out,int echo[],int n); 			//for constructor
 	int out;								//out pin
 	int *echo;								//echo pin list
+	int n;									//number of el
 };
